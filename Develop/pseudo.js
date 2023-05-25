079dbf64882fe6cb15621edbfb61e0d9dfedc9f1
Brian Sellers <bsellers3013@gmail.com>
2023-05-25 07:05:16 -0500

    added date via JS

--- Develop/pseudo.js
+++ Develop/pseudo.js
@@ -9,4 +9,13 @@
     //With current time will determine what color block is needed. Add class based upon time of day. 
 
 //Call on timer function to begin application. 
+
+console.log(dayjs().format("MMM D, YYYY"));
+var currentTimeEl = $("#current-time");
+currentTimeEl.text(dayjs().format("MMM D, YYYY"));
  
+// let saveEl = document.getElementById
+// let save = 0
+
+// function save() {
+//     console.log(saveBtn)
